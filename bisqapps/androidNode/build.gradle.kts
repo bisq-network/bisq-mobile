2259010d893e6ce9bbbc9eed9cfeda8a26ad99cb
Rodrigo Varela <rodvar@gmail.com>
2024-11-06 18:09:39 +1100

    - migrate  in protobuf needed filees and using proto lite to build the generated classes for the runtime code to use

--- bisqapps/androidNode/build.gradle.kts
+++ bisqapps/androidNode/build.gradle.kts
@@ -1,11 +1,13 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.google.protobuf.gradle.*
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.protobuf)
 }
 
 version = project.findProperty("node.android.version") as String
@@ -24,12 +26,45 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
         }
+        val androidMain by getting {
+            androidMain.dependencies {
+                implementation(compose.preview)
+                implementation(libs.androidx.activity.compose)
+            }
+            kotlin.srcDirs(
+                "src/androidMain/kotlin",
+                "${layout.buildDirectory}/generated/source/proto/debug/java",
+                "${layout.buildDirectory}/generated/source/proto/release/java"
+            )
+        }
     }
 }
 
 android {
     namespace = "network.bisq.mobile.node"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets {
+        getByName("main") {
+            proto {
+                srcDir("src/androidMain/proto")
+            }
+            java.srcDirs(
+                "src/layout.buildDirectory/kotlin",
+                "${layout.buildDirectory}/generated/source/proto/debug/java",
+                "${layout.buildDirectory}/generated/source/proto/release/java"
+            )
+        }
+    }
+//    sourceSets {
+//        getByName("main") {
+//            proto {
+//                srcDir("src/androidMain/proto")
+//            }
+//            println("proto files generated in ${protobuf.generatedFilesBaseDir}/java")
+//            java.srcDir("build/generated/source/proto/release/java")
+//        }
+//    }
 
     defaultConfig {
         applicationId = "network.bisq.mobile.node"
@@ -43,6 +78,11 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            // the following exclude are needed to avoid protobuf hanging build when merging release resources for java
+            // Exclude the conflicting META-INF files
+            excludes.add("META-INF/versions/9/OSGI-INF/MANIFEST.MF")
+            excludes.add("META-INF/DEPENDENCIES")
+            pickFirsts.add("**/protobuf/**/*.class")
         }
     }
     buildTypes {
@@ -59,9 +99,40 @@ android {
     }
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:4.28.2"
+    }
+    plugins {
+        create("javalite") {
+            artifact = "com.google.protobuf:protoc-gen-javalite:3.0.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                create("java") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
 dependencies {
     implementation(project(":shared:presentation"))
     implementation(project(":shared:domain"))
     debugImplementation(compose.uiTooling)
+
+    // bisq2 core dependencies
+
+    // protobuf
+    implementation(libs.protobuf.lite)
+
+//    implementation(libs.protobuf.java)
+    implementation(libs.protobuf.gradle.plugin)
+    implementation(libs.protoc)
 }
 
+//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+//    dependsOn("generateProto")
+//}
