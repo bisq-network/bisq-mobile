554ecd66352159f0fd8b9f71f12c4b957dafeb19
Rodrigo Varela <rodvar@gmail.com>
2024-11-13 09:29:10 +1100

    - capable of creating profile if non existent and gets persisted

--- bisqapps/androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/presentation/MainNodePresenter.kt
+++ bisqapps/androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/presentation/MainNodePresenter.kt
@@ -13,32 +13,45 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.security.Security
 import android.os.Process
+import bisq.android.main.user_profile.UserProfileModel
 import bisq.common.observable.Observable
 import bisq.user.identity.UserIdentityService
 import bisq.application.State
 import bisq.chat.ChatChannelDomain
 import bisq.common.locale.LanguageRepository
 import bisq.network.p2p.services.data.BroadcastResult
+import bisq.security.DigestUtil
+import bisq.security.SecurityService
+import bisq.user.identity.NymIdGenerator
+import bisq.user.identity.UserIdentity
+import bisq.user.profile.UserProfile
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import network.bisq.mobile.android.node.service.AndroidApplicationService
 import java.util.Optional
+import java.util.concurrent.CompletableFuture
 import kotlin.jvm.optionals.getOrElse
 import kotlin.random.Random
 
 class MainNodePresenter(greetingRepository: GreetingRepository): MainPresenter(greetingRepository) {
+    companion object {
+        private const val AVATAR_VERSION = 0
+    }
     private val logMessage: Observable<String> = Observable("")
     val state = Observable(State.INITIALIZE_APP)
     private val shutDownErrorMessage = Observable<String>()
     private val startupErrorMessage = Observable<String>()
+
+    private val profileModel = UserProfileModel()
 
     private val coroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
     private val loggingScope = CoroutineScope(Dispatchers.IO)
 
     private lateinit var applicationService: AndroidApplicationService
     private lateinit var userIdentityService: UserIdentityService
-//    private val userProfileController: UserProfileController
+    private lateinit var securityService: SecurityService
+
 
     init {
         // TODO move to application once DI setup gets merged
@@ -63,6 +76,7 @@ class MainNodePresenter(greetingRepository: GreetingRepository): MainPresenter(g
         log("Path for files dir $filesDirsPath")
         applicationService = AndroidApplicationService(filesDirsPath)
         userIdentityService = applicationService.userService.userIdentityService
+        securityService = applicationService.securityService
 
         launchServices()
     }
@@ -83,15 +97,15 @@ class MainNodePresenter(greetingRepository: GreetingRepository): MainPresenter(g
             printLanguageCode()
 
             // At the moment is nor persisting the profile so it will create one on each run
-//            if (userIdentityService.userIdentities.isEmpty()) {
-//                //createUserIfNoneExist();
-//                userProfileController.initialize()
-//
-//                // mock profile creation and wait until done.
-//                userProfileController.createUserProfile().join()
-//                appendLog("Created profile for user", "")
-//            }
-//            printUserProfiles()
+            if (userIdentityService.userIdentities.isEmpty()) {
+                //createUserIfNoneExist();
+                initializeUserService()
+
+                // mock profile creation and wait until done.
+                createUserProfile("Android user " + Random(4234234).nextInt(100)).join()
+                logMessage.set("Created profile for user")
+            }
+            printUserProfiles()
 
 //            observeNetworkState() // prints to screen
 //            observeNumConnections()
@@ -104,6 +118,60 @@ class MainNodePresenter(greetingRepository: GreetingRepository): MainPresenter(g
 //
 //            sendRandomMessagesEvery(60 * 100)
         }
+    }
+
+    private fun printUserProfiles() {
+        applicationService.userService.userIdentityService.userIdentities.stream()
+            .map { obj: UserIdentity -> obj.userProfile }
+            .map { userProfile: UserProfile -> userProfile.userName + " [" + userProfile.nym + "]" }
+            .forEach { userName: String -> logMessage.set("My profile $userName") }
+    }
+
+    private fun createUserProfile(nickName: String): CompletableFuture<UserIdentity> {
+        // UI can listen to that state change and show busy animation
+        profileModel.isBusy.set(true)
+        return userIdentityService.createAndPublishNewUserProfile(
+            nickName,
+            profileModel.keyPair,
+            profileModel.pubKeyHash,
+            profileModel.proofOfWork,
+            AVATAR_VERSION,
+            profileModel.terms.get(),
+            profileModel.statement.get()
+        )
+            .whenComplete { userIdentity: UserIdentity?, throwable: Throwable? ->
+                // UI can listen to that state change and stop busy animation and show close button
+                profileModel.isBusy.set(false)
+            }
+    }
+
+    private fun initializeUserService() {
+        val userIdentities = userIdentityService.userIdentities
+        if (userIdentities.isEmpty()) {
+            // Generate
+            onGenerateKeyPair()
+        } else {
+            // If we have already a user profile we don't do anything. Leave it to the parent
+            // controller to skip and not even create initialize controller.
+            logMessage.set("We have already a user profile.")
+        }
+    }
+
+    private fun onGenerateKeyPair() {
+        val keyPair = securityService.keyBundleService.generateKeyPair()
+        profileModel.keyPair = keyPair
+        val pubKeyHash = DigestUtil.hash(keyPair.public.encoded)
+        profileModel.pubKeyHash = pubKeyHash
+        val proofOfWork = userIdentityService.mintNymProofOfWork(pubKeyHash)
+        profileModel.proofOfWork = proofOfWork
+        val powSolution = proofOfWork.solution
+        val nym = NymIdGenerator.generate(pubKeyHash, powSolution)
+        profileModel.nym.set(nym) // nym will be created on demand from pubKeyHash and pow
+        // CatHash is in desktop, needs to be reimplemented or the javafx part extracted and refactored into a non javafx lib
+        //  Image image = CatHash.getImage(pubKeyHash,
+        //                                powSolution,
+        //                                CURRENT_AVATARS_VERSION,
+        //                                CreateProfileModel.CAT_HASH_IMAGE_SIZE);
     }
 
     private fun printDefaultKeyId() {
