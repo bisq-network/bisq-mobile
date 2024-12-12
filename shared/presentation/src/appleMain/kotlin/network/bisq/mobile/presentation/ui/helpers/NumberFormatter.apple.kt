ba92bb30fb012897caba0cc8aaf599f836e1272c
Rodrigo Varela <rodvar@gmail.com>
2024-12-12 11:04:33 +1100

    - delete unnecessary appleMain dir

--- shared/presentation/src/appleMain/kotlin/network/bisq/mobile/presentation/ui/helpers/NumberFormatter.apple.kt
+++
@@ -1,4 +0,0 @@
-package network.bisq.mobile.presentation.ui.helpers
-
-// actual val numberFormatter: network.bisq.mobile.presentation.ui.helpers.NumberFormatter
-    // get() = TODO("Not yet implemented")
