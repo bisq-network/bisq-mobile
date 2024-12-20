52ab84ee0b7d755d0af406a35bfb9bb1c2e46d7e
HenrikJannsen <boilingfrog@gmx.com>
2024-12-20 18:15:31 +0700

    Add i18n support using java property files. Not working yet on ios. Processing of the file content need more work (manual linebreaks not detected yet) and testing.

--- shared/domain/src/iosMain/kotlin/network/bisq/mobile/domain/PlatformDomainAbstractions.ios.kt
+++ shared/domain/src/iosMain/kotlin/network/bisq/mobile/domain/PlatformDomainAbstractions.ios.kt
@@ -5,14 +5,21 @@ package network.bisq.mobile.domain
 import com.russhwolf.settings.ExperimentalSettingsImplementation
 import com.russhwolf.settings.KeychainSettings
 import com.russhwolf.settings.Settings
-import kotlinx.cinterop.*
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.MemScope
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.refTo
+import kotlinx.cinterop.usePinned
 import kotlinx.serialization.Serializable
+import platform.Foundation.NSBundle
 import platform.Foundation.NSData
-import platform.UIKit.UIImage
+import platform.Foundation.NSString
 import platform.Foundation.create
-import platform.UIKit.UIImagePNGRepresentation
-
+import platform.Foundation.stringWithContentsOfFile
 import platform.UIKit.UIDevice
+import platform.UIKit.UIImage
+import platform.UIKit.UIImagePNGRepresentation
 import platform.posix.memcpy
 
 @OptIn(ExperimentalSettingsImplementation::class)
@@ -27,6 +34,12 @@ class IOSPlatformInfo: PlatformInfo {
 }
 
 actual fun getPlatformInfo(): PlatformInfo = IOSPlatformInfo()
+
+actual fun loadFromResources(fileName: String): String {
+    val path = NSBundle.mainBundle.pathForResource(fileName, "txt")
+        ?: throw IllegalArgumentException("File not found: $fileName")
+    return NSString.stringWithContentsOfFile(path) as String
+}
 
 @Serializable(with = PlatformImageSerializer::class)
 actual class PlatformImage(val image: UIImage) {
