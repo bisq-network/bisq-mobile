09124a7ccc9531ccd9b7d9a643fd4726091ed98c
Rodrigo Varela <rodvar@gmail.com>
2024-12-23 11:51:06 +1100

    - fix crash when some mappings are not found/defined & defaulting to english

--- shared/domain/src/commonMain/kotlin/network/bisq/mobile/i18n/ResourceBundle.kt
+++ shared/domain/src/commonMain/kotlin/network/bisq/mobile/i18n/ResourceBundle.kt
@@ -7,13 +7,30 @@ import network.bisq.mobile.utils.getLogger
 class ResourceBundle(val bundleName: String, val languageCode: String, val map: Map<String, String>) {
     companion object {
         fun getBundle(bundleName: String, languageCode: String): ResourceBundle {
+            var map: Map<String, String>? = null
+            try {
+                map = loadMappings(bundleName, languageCode)
+            } catch (e: Exception) {
+                getLogger("ResourceBundle").e("Failed to load bundle i18n for $languageCode", e)
+                if (map == null) {
+                    getLogger("ResourceBundle").i("Defaulting to english")
+                    map = loadMappings(bundleName,"en")
+                }
+            } finally {
+                if (map == null) {
+                    throw IllegalArgumentException("Could not find mappings for bundle $bundleName and language $languageCode")
+                }
+                return ResourceBundle(bundleName, languageCode, map)
+            }
+        }
+
+        private fun loadMappings(bundleName: String, languageCode: String): Map<String, String> {
             val code = if (languageCode.lowercase() == "en") "" else "_$languageCode"
             // We must use a sub directory as otherwise it would get shadowed with the resources from bisq 2 i18n jar in node
             val fileName = "mobile/$bundleName$code.properties"
             val ts = Clock.System.now()
-            val map: Map<String, String> = loadProperties(fileName)
             getLogger("ResourceBundle").i("Loading $bundleName took ${Clock.System.now() - ts}")
-            return ResourceBundle(bundleName, languageCode, map)
+            return loadProperties(fileName)
         }
 
         // Preprocess content to merge lines ending with '\n\n\'
