3032a61ed687597210890c8ba865f1b99ef1db4c
HenrikJannsen <boilingfrog@gmx.com>
2025-01-14 13:31:15 +0700

    Check if paymentProof is set only for mainchain. Ensure we sent null and not empty string if not set on LN.

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/open_trades/selected/states/SellerState3aPresenter.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/open_trades/selected/states/SellerState3aPresenter.kt
@@ -1,11 +1,9 @@
 package network.bisq.mobile.presentation.ui.uicases.open_trades.selected.states
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import network.bisq.mobile.domain.data.BackgroundDispatcher
 import network.bisq.mobile.domain.data.replicated.presentation.open_trades.TradeItemPresentationModel
 import network.bisq.mobile.domain.service.trades.TradesServiceFacade
 import network.bisq.mobile.presentation.BasePresenter
@@ -19,8 +17,8 @@ class SellerState3aPresenter(
 
     val selectedTrade: StateFlow<TradeItemPresentationModel?> = tradesServiceFacade.selectedTrade
 
-    private var _paymentProof: MutableStateFlow<String> = MutableStateFlow("")
-    val paymentProof: StateFlow<String> get() = _paymentProof
+    private var _paymentProof: MutableStateFlow<String?> = MutableStateFlow(null)
+    val paymentProof: StateFlow<String?> get() = _paymentProof
 
     private var _buttonEnabled: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val buttonEnabled: StateFlow<Boolean> get() = _buttonEnabled
@@ -33,24 +31,30 @@ class SellerState3aPresenter(
         val openTradeItemModel = tradesServiceFacade.selectedTrade.value!!
         val paymentMethod = openTradeItemModel.bisqEasyTradeModel.contract.baseSidePaymentMethodSpec.paymentMethod
         isLightning = paymentMethod == "LN"
-        _buttonEnabled.value = isLightning || paymentProof.value.isNotEmpty()
+        updateButtonEnableState()
     }
 
     override fun onViewUnattaching() {
         job?.cancel()
         job = null
-        _paymentProof.value = ""
+        _paymentProof.value = null
     }
 
     fun onPaymentProofInput(value: String) {
-        _paymentProof.value = value.trim()
-        _buttonEnabled.value = isLightning || paymentProof.value.isNotEmpty()
+        _paymentProof.value = value.trim().ifEmpty { null }
+        updateButtonEnableState()
     }
 
     fun onConfirmedBtcSent() {
-        require(paymentProof.value.isNotEmpty())
+        if (!isLightning) {
+            require(paymentProof.value != null)
+        }
         job = backgroundScope.launch {
             tradesServiceFacade.sellerConfirmBtcSent(paymentProof.value)
         }
+    }
+
+    private fun updateButtonEnableState() {
+        _buttonEnabled.value = isLightning || paymentProof.value != null
     }
 }
