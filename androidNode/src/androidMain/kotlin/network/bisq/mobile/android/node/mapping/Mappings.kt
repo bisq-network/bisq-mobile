b22df4da6279c402f02cf770f3cd2ae863b2de75
Rodrigo Varela <rodvar@gmail.com>
2025-02-03 12:46:00 +1100

    - default to bisq core settings value for DEFAULT_NUM_DAYS_AFTER_REDACTING_TRADE_DATA

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/mapping/Mappings.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/mapping/Mappings.kt
@@ -1120,7 +1120,6 @@ class Mappings {
 
     object SettingsMapping {
         // toPojo method not implemented as we do not have a settings value object in the domain
-        private const val DEFAULT_DAYS_AFTER_REDACTING_TRADE_DATA = 0
         fun from(settingsService: SettingsService): SettingsVO {
             return SettingsVO(
                 settingsService.isTacAccepted.get(),
@@ -1131,7 +1130,7 @@ class Mappings {
                 settingsService.maxTradePriceDeviation.get(),
                 settingsService.useAnimations.get(),
                 MarketMapping.fromBisq2Model(settingsService.selectedMarket.get()),
-                DEFAULT_DAYS_AFTER_REDACTING_TRADE_DATA
+                SettingsService.DEFAULT_NUM_DAYS_AFTER_REDACTING_TRADE_DATA
             )
         }
     }
