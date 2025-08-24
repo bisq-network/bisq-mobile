052afe9124b79e98e7db1eb9f4e793746d1911b6
Salotto <salotto.dev@proton.me>
2025-08-24 17:46:38 -0300

    Rearchitect font handling to be theme-driven

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/components/atoms/AutoResizeText.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/components/atoms/AutoResizeText.kt
@@ -2,7 +2,6 @@ package network.bisq.mobile.presentation.ui.components.atoms
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -10,14 +9,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-import network.bisq.mobile.presentation.ui.components.atoms.BisqText.fontFamilyBold
-import network.bisq.mobile.presentation.ui.components.atoms.BisqText.fontFamilyLight
-import network.bisq.mobile.presentation.ui.components.atoms.BisqText.fontFamilyMedium
-import network.bisq.mobile.presentation.ui.components.atoms.BisqText.fontFamilyRegular
 import network.bisq.mobile.presentation.ui.theme.BisqTheme
 
 /**
@@ -28,33 +24,16 @@ import network.bisq.mobile.presentation.ui.theme.BisqTheme
 fun AutoResizeText(
     text: String,
     color: Color = BisqTheme.colors.white,
-    fontSize: FontSize = FontSize.BASE,
-    fontWeight: FontWeight = FontWeight.REGULAR,
+    textStyle: TextStyle = BisqTheme.typography.baseRegular,
     textAlign: TextAlign = TextAlign.Start,
-    lineHeight: TextUnit = TextUnit.Unspecified,
+    lineHeight: TextUnit = BisqText.getDefaultLineHeight(textStyle.fontSize),
     maxLines: Int = 1,
     overflow: TextOverflow = TextOverflow.Clip,
     minimumFontSize: TextUnit = 10.sp,
     modifier: Modifier = Modifier,
 ) {
+    var fontSize by remember { mutableStateOf(textStyle.fontSize) }
     var readyToDraw by remember(text, fontSize, maxLines, overflow) { mutableStateOf(false) }
-    var determinedFontSize by remember(text, fontSize)  { mutableStateOf(fontSize.size) }
-    val determinedLineHeight by remember {
-        derivedStateOf {
-            if (lineHeight == TextUnit.Unspecified) {
-                determinedFontSize * 1.15f
-            } else {
-                lineHeight
-            }
-        }
-    }
-
-    val fontFamily = when (fontWeight) {
-        FontWeight.LIGHT -> fontFamilyLight()
-        FontWeight.REGULAR -> fontFamilyRegular()
-        FontWeight.MEDIUM -> fontFamilyMedium()
-        FontWeight.BOLD -> fontFamilyBold()
-    }
 
     Text(
         text = text,
@@ -62,16 +41,15 @@ fun AutoResizeText(
             if (readyToDraw) drawContent()
         },
         color = color,
-        fontSize = determinedFontSize,
-        fontFamily = fontFamily,
+        style = textStyle,
         textAlign = textAlign,
-        lineHeight = determinedLineHeight,
+        lineHeight = lineHeight,
         maxLines = maxLines,
         overflow = overflow,
         onTextLayout = { textLayoutResult ->
-            if (textLayoutResult.hasVisualOverflow && determinedFontSize > minimumFontSize) {
-                val next = determinedFontSize * 0.9f
-                determinedFontSize = if (next < minimumFontSize) minimumFontSize else next
+            if (textLayoutResult.hasVisualOverflow && fontSize > minimumFontSize) {
+                val next = fontSize * 0.9f
+                fontSize = if (next < minimumFontSize) minimumFontSize else next
             } else {
                 readyToDraw = true
             }
