590c54add5f5f893aaaf7495af63cede35bb319b
wodoro <woodencore@proton.me>
2025-08-25 18:34:14 +0330

    fix: apply CRAI suggestions

--- shared/domain/src/commonMain/kotlin/network/bisq/mobile/domain/data/model/TradeReadState.kt
+++ shared/domain/src/commonMain/kotlin/network/bisq/mobile/domain/data/model/TradeReadState.kt
@@ -9,6 +9,7 @@ data class TradeReadState(
 ): BaseModel() {
     init {
         require(tradeId.isNotBlank()) { "TradeReadState must have a non-blank tradeId" }
+        require(readCount >= 0) { "TradeReadState.readCount must be >= 0" }
         id = tradeId
     }
 
@@ -19,9 +20,9 @@ data class TradeReadState(
     }
 
     override fun hashCode(): Int {
-        var result = super.hashCode()
-        result = 31 * result + readCount
+        var result = 17
         result = 31 * result + tradeId.hashCode()
+        result = 31 * result + readCount
         return result
     }
 }
