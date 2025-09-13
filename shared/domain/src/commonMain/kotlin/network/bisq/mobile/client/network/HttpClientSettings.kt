42cf89fdbcad490ebd7d617328cfb262475490c7
wodoro <woodencore@proton.me>
2025-09-13 05:43:13 +0330

    fix: avoid nullable boolean

--- shared/domain/src/commonMain/kotlin/network/bisq/mobile/client/network/HttpClientSettings.kt
+++ shared/domain/src/commonMain/kotlin/network/bisq/mobile/client/network/HttpClientSettings.kt
@@ -9,16 +9,20 @@ import network.bisq.mobile.domain.data.replicated.common.network.AddressVO
 data class HttpClientSettings(
     val apiUrl: String?,
     val proxyUrl: String?,
-    val isExternalProxyTor: Boolean?,
+    val isExternalProxyTor: Boolean = false,
 ) {
     companion object {
         fun from(settings: Settings?) =
-            HttpClientSettings(settings?.bisqApiUrl, settings?.proxyUrl, settings?.isExternalProxyTor)
+            HttpClientSettings(
+                settings?.bisqApiUrl,
+                settings?.proxyUrl,
+                settings?.isExternalProxyTor ?: false,
+            )
     }
 
     fun bisqProxyConfig(): BisqProxyConfig? {
         // isExternalProxyTor is for future non tor proxy support
-        if (this.proxyUrl?.isBlank() == false && this.isExternalProxyTor == true) {
+        if (this.proxyUrl?.isBlank() == false && this.isExternalProxyTor) {
             val address = AddressVO.from(this.proxyUrl)
             if (address != null) {
                 return BisqProxyConfig(
