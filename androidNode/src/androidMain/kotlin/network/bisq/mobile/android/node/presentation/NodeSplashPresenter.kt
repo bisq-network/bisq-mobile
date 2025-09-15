889a41b6af129e4221429c991a4292006af6c821
HenrikJannsen <boilingfrog@gmx.com>
2025-09-15 11:29:23 +0700

    Guard against client dependencies.
    Should be fixed properly by refactoring it into a abstract class with a custom Client subclass.

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/presentation/NodeSplashPresenter.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/presentation/NodeSplashPresenter.kt
@@ -1,5 +1,7 @@
 package network.bisq.mobile.android.node.presentation
 
+import android.app.Activity
+import network.bisq.mobile.android.node.NodeApplicationLifecycleController
 import network.bisq.mobile.domain.data.model.Settings
 import network.bisq.mobile.domain.data.repository.SettingsRepository
 import network.bisq.mobile.domain.data.repository.UserRepository
@@ -18,6 +20,7 @@ class NodeSplashPresenter(
     settingsRepository: SettingsRepository,
     settingsServiceFacade: SettingsServiceFacade,
     languageServiceFacade: LanguageServiceFacade,
+    private val nodeApplicationLifecycleController: NodeApplicationLifecycleController
 ) : SplashPresenter(
     mainPresenter,
     applicationBootstrapFacade,
@@ -35,8 +38,23 @@ class NodeSplashPresenter(
         return false
     }
 
+    override suspend fun isClientAndHasNoConnectivity(): Boolean {
+        return false
+    }
+
+    // TODO refactor not not need that
+    override suspend fun handleDemoModeForClient() {
+        // Do nothing, only used in client mode
+    }
+
     override suspend fun hasConnectivity(): Boolean {
         return mainPresenter.isConnected()
+    }
+
+    // TODO Would be better if all such code is extracted in a ClientSplashPresenter making such guards not needed
+    override fun navigateToTrustedNodeSetup() {
+        log.w { "navigateToTrustedNodeSetup called on node app. This should never happen." }
+        // Ensure we never call TrustedNodeSetup in node mode
     }
 
     /**
@@ -46,12 +64,12 @@ class NodeSplashPresenter(
     override fun restartApp() {
         log.i { "User requested app restart from failed state - restarting application" }
 
-        (mainPresenter as NodeMainPresenter).restartApp()
+        nodeApplicationLifecycleController.restartApp(view as Activity)
     }
 
     override fun shutdownApp() {
         log.i { "User requested app shutdown from failed state" }
 
-        (mainPresenter as NodeMainPresenter).shutdownApp()
+        nodeApplicationLifecycleController.shutdownApp()
     }
 }
