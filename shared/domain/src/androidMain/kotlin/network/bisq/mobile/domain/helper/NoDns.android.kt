c819d06e20db71f48d0e576595788a5c876892ba
wodoro <woodencore@proton.me>
2025-09-15 12:07:14 +0330

    chore: add comment on where nodns comes from

--- shared/domain/src/androidMain/kotlin/network/bisq/mobile/domain/helper/NoDns.android.kt
+++ shared/domain/src/androidMain/kotlin/network/bisq/mobile/domain/helper/NoDns.android.kt
@@ -3,6 +3,12 @@ package network.bisq.mobile.domain.helper
 import okhttp3.Dns
 import java.net.InetAddress
 
+/**
+ * Prevent DNS requests.
+ * Important when proxying all requests over Tor to not leak DNS queries.
+ *
+ * taken from https://github.com/f-droid/fdroidclient/blob/7803475afa8a04d4db42ad682366890f79226e72/libs/download/src/androidMain/kotlin/org/fdroid/download/HttpManager.kt#L67-L75
+ */
 class NoDns : Dns {
     override fun lookup(hostname: String): List<InetAddress> {
         return listOf(InetAddress.getByAddress(hostname, ByteArray(4)))
