de1accd05794db9f3fe4506e649a2c64311d6e7e
wodoro <woodencore@proton.me>
2025-09-25 12:20:31 +0330

    fix: revert the unnecessary change to build gradle

--- shared/presentation/build.gradle.kts
+++ shared/presentation/build.gradle.kts
@@ -63,7 +63,7 @@ kotlin {
             
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
-            api(libs.navigation.compose) // to allow use of NavOptionsBuilder in ClientSplashPresenter
+            implementation(libs.navigation.compose)
             implementation(libs.bignum)
             implementation(libs.coil.compose)
         }
