c674367aa5713e6aea02362cbf58b9cded39129b
nostrbuddha <nostrbuddha@proton.me>
2025-09-27 13:39:49 +0530

    - Added accessibility id as modifier.semantics(contentDescription) to tag elemenets in testing

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/components/molecules/TopBar.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/components/molecules/TopBar.kt
@@ -24,6 +24,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -155,6 +157,7 @@ fun TopBar(
                                 presenter.navigateToUserProfile()
                             }
                         }
+                        .semantics { contentDescription = "top_bar_avatar" }
 
                     BisqGap.H1()
                     val uniqueAvatar by presenter.uniqueAvatar.collectAsState()
