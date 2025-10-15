93d6f3c01917b70a207e45b66f499f1e40081c68
axpoems <145597137+axpoems@users.noreply.github.com>
2025-10-15 13:00:35 +0200

    Apply CRAI suggestion

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/utils/NodeVersionProvider.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/utils/NodeVersionProvider.kt
@@ -5,10 +5,13 @@ import network.bisq.mobile.domain.utils.VersionProvider
 import network.bisq.mobile.i18n.i18n
 
 class NodeVersionProvider() : VersionProvider {
-    override fun getVersionInfo(isDemo: Boolean, isIOS: Boolean): String {
+    private fun getAppName(isDemo: Boolean): String {
         val demo = if (isDemo) "-demo-" else ""
-        val appName = demo + BuildNodeConfig.APP_NAME
+        return demo + BuildNodeConfig.APP_NAME
+    }
 
+    override fun getVersionInfo(isDemo: Boolean, isIOS: Boolean): String {
+        val appName = getAppName(isDemo)
         return "mobile.resources.versionDetails.node".i18n(
             appName,
             BuildNodeConfig.APP_VERSION,
@@ -18,8 +21,7 @@ class NodeVersionProvider() : VersionProvider {
     }
 
     override fun getAppNameAndVersion(isDemo: Boolean, isIOS: Boolean): String {
-        val demo = if (isDemo) "-demo-" else ""
-        val appName = demo + BuildNodeConfig.APP_NAME
+        val appName = getAppName(isDemo)
         return "$appName v${BuildNodeConfig.APP_VERSION}"
     }
 }
