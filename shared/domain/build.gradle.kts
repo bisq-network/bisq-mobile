2d4e0b452a65440563a70569fd2e3001d939e874
wodoro <woodencore@proton.me>
2025-10-17 08:53:26 +0330

    fix: add ktor nework to domain common for KmpTorService

--- shared/domain/build.gradle.kts
+++ shared/domain/build.gradle.kts
@@ -114,6 +114,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.kotlinx.serialization.core)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.network)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.ktor.client.websockets)
