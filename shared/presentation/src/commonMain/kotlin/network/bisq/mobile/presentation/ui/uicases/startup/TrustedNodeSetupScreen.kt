459c92ffaf4dfe0fb19479d3d59b7b1367d745cf
wodoro <woodencore@proton.me>
2025-10-22 09:03:01 +0330

    fix: show paste button

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/startup/TrustedNodeSetupScreen.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/startup/TrustedNodeSetupScreen.kt
@@ -105,6 +105,7 @@ fun TrustedNodeSetupScreen(isWorkflow: Boolean = true) {
                     value = apiUrl,
                     placeholder = apiUrlPrompt,
                     disabled = isLoading,
+                    showPaste = true,
                     validation = {
                         presenter.validateApiUrl(
                             it, selectedProxyOption,
