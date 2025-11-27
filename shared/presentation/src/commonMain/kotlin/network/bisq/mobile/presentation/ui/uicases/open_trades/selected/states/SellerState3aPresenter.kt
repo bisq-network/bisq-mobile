bf47e64a797a0e4042a911e3a3312d550f4c9789
wodoro <woodencore@proton.me>
2025-11-27 23:54:47 +0330

    fix: context switch for sellerConfirmBtcSent

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/open_trades/selected/states/SellerState3aPresenter.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/open_trades/selected/states/SellerState3aPresenter.kt
@@ -1,11 +1,8 @@
 package network.bisq.mobile.presentation.ui.uicases.open_trades.selected.states
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.withContext
 import network.bisq.mobile.domain.data.replicated.presentation.open_trades.TradeItemPresentationModel
 import network.bisq.mobile.domain.service.trades.TradesServiceFacade
 import network.bisq.mobile.presentation.BasePresenter
@@ -81,9 +78,7 @@ class SellerState3aPresenter(
         }
         launchUI {
             showLoading()
-            withContext(Dispatchers.IO) {
-                tradesServiceFacade.sellerConfirmBtcSent(paymentProof.value)
-            }
+            tradesServiceFacade.sellerConfirmBtcSent(paymentProof.value)
             hideLoading()
             setShowInvalidAddressDialog(false)
         }
