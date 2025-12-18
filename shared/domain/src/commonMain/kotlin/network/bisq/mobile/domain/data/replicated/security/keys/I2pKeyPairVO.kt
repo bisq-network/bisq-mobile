0c4131328fadf2c574b6410e1fa7aaeda255addb
rodvar <1040902+rodvar@users.noreply.github.com>
2025-12-18 16:48:06 +0800

    - fix todo of missing I2p VO impl causing crash in connect apps

--- shared/domain/src/commonMain/kotlin/network/bisq/mobile/domain/data/replicated/security/keys/I2pKeyPairVO.kt
+++ shared/domain/src/commonMain/kotlin/network/bisq/mobile/domain/data/replicated/security/keys/I2pKeyPairVO.kt
@@ -2,6 +2,8 @@ package network.bisq.mobile.domain.data.replicated.security.keys
 
 import kotlinx.serialization.Serializable
 
-// TODO implement proper properties
 @Serializable
-data class I2pKeyPairVO(val privateKeyEncoded: String, val publicKeyEncoded: String, val onionAddress: String)
+data class I2pKeyPairVO(
+    val identityBytes: String,      // Base64 encoded byte[]
+    val destinationBytes: String    // Base64 encoded byte[]
+)
