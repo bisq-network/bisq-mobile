a0a00ee6457db12ee3dc57746d3f00de03fe5432
HenrikJannsen <boilingfrog@gmx.com>
2026-01-21 12:59:40 +0700

    Remove password based authentication as we support TLS now

--- apps/clientApp/src/commonMain/kotlin/network/bisq/mobile/client/common/domain/httpclient/HttpClientService.kt
+++ apps/clientApp/src/commonMain/kotlin/network/bisq/mobile/client/common/domain/httpclient/HttpClientService.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.UserAgent
-import io.ktor.client.plugins.api.createClientPlugin
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.HttpRequestBuilder
@@ -15,11 +14,7 @@ import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.content.ByteArrayContent
-import io.ktor.http.content.OutgoingContent
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.utils.io.ByteChannel
-import io.ktor.utils.io.readRemaining
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.BufferOverflow
@@ -31,13 +26,10 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.launch
-import kotlinx.datetime.Clock
-import kotlinx.io.readByteArray
 import kotlinx.serialization.json.Json
 import network.bisq.mobile.client.common.domain.httpclient.exception.UnauthorizedApiAccessException
 import network.bisq.mobile.client.common.domain.sensitive_settings.SensitiveSettingsRepository
 import network.bisq.mobile.client.common.domain.utils.createHttpClient
-import network.bisq.mobile.crypto.getSha256
 import network.bisq.mobile.domain.PlatformType
 import network.bisq.mobile.domain.getPlatformInfo
 import network.bisq.mobile.domain.service.ServiceFacade
@@ -187,7 +179,6 @@ class HttpClientService(
             log.w { "Sanitized baseUrl from '$rawBase' to '$baseUrl'" }
         }
         log.d { "HttpClient baseUrl set to $baseUrl" }
-        val password = clientSettings.password
         return createHttpClient(
             host = host,
             tlsFingerprint = tlsFingerprint,
@@ -224,115 +215,6 @@ class HttpClientService(
                     }
                 }
             }
-            install(
-                createClientPlugin("HttpApiAuthPlugin") {
-                    transformRequestBody { request, content, bodyType ->
-                        var reconstructedBody: ByteArrayContent? = null
-                        if (!password.isNullOrBlank()) {
-                            val method = request.method.value
-                            val timestamp =
-                                Clock.System
-                                    .now()
-                                    .toEpochMilliseconds()
-                                    .toString()
-                            val nonce = AuthUtils.generateNonce()
-                            val normalizedPathAndQuery =
-                                AuthUtils.getNormalizedPathAndQuery(request.url.build())
-                            val bodySha256Hex =
-                                when (content) {
-                                    is OutgoingContent.ByteArrayContent -> {
-                                        val bytes = content.bytes()
-                                        if (bytes.size > MAX_BODY_SIZE_BYTES) {
-                                            throw IllegalArgumentException("Request body exceeds maximum size of $MAX_BODY_SIZE_BYTES bytes")
-                                        }
-                                        getSha256(bytes).toHexString()
-                                    }
-
-                                    is OutgoingContent.ReadChannelContent -> {
-                                        val bytes =
-                                            content
-                                                .readFrom()
-                                                .readRemaining(
-                                                    MAX_BODY_SIZE_BYTES + 1,
-                                                ) // + 1 to detect if max size has reached
-                                                .readByteArray()
-                                        if (bytes.size > MAX_BODY_SIZE_BYTES) {
-                                            throw IllegalArgumentException("Request body exceeds maximum size of $MAX_BODY_SIZE_BYTES bytes")
-                                        }
-                                        reconstructedBody =
-                                            ByteArrayContent(
-                                                bytes,
-                                                content.contentType,
-                                                content.status,
-                                            )
-                                        getSha256(bytes).toHexString()
-                                    }
-
-                                    is OutgoingContent.WriteChannelContent -> {
-                                        val channel =
-                                            ByteChannel(autoFlush = true)
-                                        try {
-                                            content.writeTo(channel)
-                                            val bytes =
-                                                channel
-                                                    .readRemaining(
-                                                        MAX_BODY_SIZE_BYTES + 1,
-                                                    ).readByteArray()
-                                            if (bytes.size > MAX_BODY_SIZE_BYTES) {
-                                                throw IllegalArgumentException("Request body exceeds maximum size of $MAX_BODY_SIZE_BYTES bytes")
-                                            }
-                                            reconstructedBody =
-                                                ByteArrayContent(
-                                                    bytes,
-                                                    content.contentType,
-                                                    content.status,
-                                                )
-                                            getSha256(bytes).toHexString()
-                                        } finally {
-                                            channel.close()
-                                        }
-                                    }
-
-                                    is String -> {
-                                        if (content.isEmpty()) {
-                                            null
-                                        } else {
-                                            val bytes =
-                                                content.encodeToByteArray()
-                                            if (bytes.size > MAX_BODY_SIZE_BYTES) {
-                                                throw IllegalArgumentException(
-                                                    "Request body exceeds maximum size of $MAX_BODY_SIZE_BYTES bytes",
-                                                )
-                                            }
-                                            getSha256(bytes).toHexString()
-                                        }
-                                    }
-
-                                    else -> null
-                                }
-
-                            val hash =
-                                AuthUtils.generateAuthHash(
-                                    password,
-                                    nonce,
-                                    timestamp,
-                                    method,
-                                    normalizedPathAndQuery,
-                                    bodySha256Hex,
-                                )
-
-                            request.headers.append("AUTH-TOKEN", hash)
-                            request.headers.append("AUTH-TS", timestamp)
-                            request.headers.append("AUTH-NONCE", nonce)
-                        }
-                        reconstructedBody
-                            ?: when (content) {
-                                is OutgoingContent -> content
-                                else -> null // transformation not applicable
-                            }
-                    }
-                },
-            )
         }
     }
 }
